8795aa016330af79ae5d1e3f8aad7d985f065ebb
Stitch <97808356+stitcharg@users.noreply.github.com>
2023-11-29 08:47:52 -0300

    Preparando todo para prod

--- infrastructure/certificates.ts
+++ infrastructure/certificates.ts
@@ -1,19 +1,39 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+import { ENV_DEV, ENV_PROD } from './consts'
 
 const config = new pulumi.Config();
 const configImagesDomain = config.require("images");
 const configAPIHost = config.require("api");
 
-const sandboxImageCertificate = new aws.acm.Certificate("certificate-images-sandbox", {
-    domainName: configImagesDomain,
-    validationMethod: "DNS",
-});
+let sandboxImageCertificate, sandboxApiCertificate;
+const nameImages = "certificate-images-sandbox";
+const nameApi = "certificate-api-sandbox";
 
-const sandboxApiCertificate = new aws.acm.Certificate("certificate-api-sandbox", {
-    domainName: configAPIHost,
-    validationMethod: "DNS",
-});
+if (ENV_DEV) {
+    sandboxImageCertificate = new aws.acm.Certificate(nameImages, {
+        domainName: configImagesDomain,
+        validationMethod: "DNS",
+    });
+} else {
+    sandboxImageCertificate = new aws.acm.Certificate(nameImages, {
+        domainName: configImagesDomain,
+        validationMethod: "DNS",
+    }, { import: "arn:aws:acm:us-east-1:REDACTED:certificate/9a4c63c7-ba6a-460c-828a-2ac49192b88a"});
+}
+
+if (ENV_DEV) {
+    sandboxApiCertificate = new aws.acm.Certificate(nameApi, {
+        domainName: configAPIHost,
+        validationMethod: "DNS",
+    });    
+} else {
+    sandboxApiCertificate = new aws.acm.Certificate(nameApi, {
+        domainName: configAPIHost,
+        validationMethod: "DNS",
+    }, { import: "arn:aws:acm:us-east-1:REDACTED:certificate/ec1e42b9-045b-494f-b45c-f7d740e6afc2"});    
+}
+
 
 export const certificateAPI = sandboxApiCertificate;
 export const certificate = sandboxImageCertificate;
