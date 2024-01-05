982933c64b1e3a34d202f659184ee919a6d0c822
Stitch <97808356+stitcharg@users.noreply.github.com>
2024-01-05 12:47:48 -0300

    Arreglada el codigo al parsear imagenes

--- argenpills-crud/src/lib/multipart-form-parser.js
+++ argenpills-crud/src/lib/multipart-form-parser.js
@@ -22,28 +22,28 @@ function saveFile(buffer, filePath) {
 function multiPartParser(event) {
     const boundary = getBoundary(event);
     const result = {};
-    event.body
-        .split(boundary)
-        .forEach(item => {
-            if (/filename=".+"/g.test(item)) {
+    event.body.split(boundary).forEach(item => {
+        if (/filename=".+"/g.test(item)) {
+            // Binary data handling
+            const fileStart = item.search(/Content-Type:\s.+/g) + item.match(/Content-Type:\s.+/g)[0].length + 4;
+            const fileBuffer = Buffer.from(item.slice(fileStart, -4), 'binary');
 
-                let fileContents = item.slice(item.search(/Content-Type:\s.+/g) + item.match(/Content-Type:\s.+/g)[0].length + 4, -4);
-                let fileBuffer = Buffer.from(fileContents, 'binary');
+            const fileName = item.match(/filename=".+"/g)[0].slice(10, -1);
+            const filePath = getFilePath(event, fileName);
+            saveFile(fileBuffer, filePath);
 
-                let fileName = item.match(/filename=".+"/g)[0].slice(10, -1);
-                let filePath = getFilePath(event, fileName);
-                saveFile(fileBuffer, filePath);
-
-                result[item.match(/name=".+";/g)[0].slice(6, -2)] = {
-                    filename: fileName,
-                    contentType: item.match(/Content-Type:\s.+/g)[0].slice(14),
-                    path: filePath
-                }
-
-            } else if (/name=".+"/g.test(item)) {
-                result[item.match(/name=".+"/g)[0].slice(6, -1)] = item.slice(item.search(/name=".+"/g) + item.match(/name=".+"/g)[0].length + 4, -4);
-            }
-        });
+            result[item.match(/name=".+";/g)[0].slice(6, -2)] = {
+                filename: fileName,
+                contentType: item.match(/Content-Type:\s.+/g)[0].slice(14),
+                path: filePath
+            };
+        } else if (/name=".+"/g.test(item)) {
+            // Text data handling
+            const textStart = item.search(/name=".+"/g) + item.match(/name=".+"/g)[0].length + 4;
+            const textData = Buffer.from(item.slice(textStart, -4), 'binary').toString('utf-8');
+            result[item.match(/name=".+"/g)[0].slice(6, -1)] = textData;
+        }
+    });
     return result;
 };
 
@@ -54,7 +54,7 @@ module.exports.parse = (event) => {
     if (event.isBase64Encoded) {
         let body = clonedEvent.body;
         let decodedFromBase64 = Buffer.from(body, 'base64');
-        clonedEvent.body = decodedFromBase64.toString('utf8');
+        clonedEvent.body = decodedFromBase64.toString('latin1');
     }
     let result = multiPartParser(clonedEvent);
 
